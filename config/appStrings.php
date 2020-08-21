19620d4e6f4da8477a3c1cae95c5162324270fd0
achalakavinda <achalakavinda25r@gmail.com>
2020-08-21 09:47:54 +0530

    add app strings configuration

---
+++ config/appStrings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'String_Brand'=> 'Brand',
+    'String_Brand_Registry'=> 'Brand',
+    'String_Brand_Show'=> 'Brand',
+    'String_Brand_Creation'=> 'Brand',
+    'String_Brand_Update'=> 'Brand'
+    ]
+?>
