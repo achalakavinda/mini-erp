4f2923ffb6955f2b7a8db8f995667350481129b9
shiromp <shiromperera@gmail.com>
2020-08-24 23:55:03 +0530

    add nullable to item code thumbnail img.

--- database/migrations/2019_09_11_171325_create_item_codes_table.php
+++ database/migrations/2019_09_11_171325_create_item_codes_table.php
@@ -36,7 +36,7 @@ class CreateItemCodesTable extends Migration
 
             $table->string('name');
             $table->string('description')->nullable();
-            $table->text('thumbnail_url')->default("http://itinerantnotes.com/blog/images/logo.png");
+            $table->text('thumbnail_url')->nullable();
 
             $table->float('unit_cost');
             $table->float('selling_price');
